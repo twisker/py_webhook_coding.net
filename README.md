2765adf2f5efdae6e4b1766fbcdade20afb8dd05
wang ying <wang.ying@sap.com>
2018-04-29 23:42:04 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # py_webhook_coding.net
 a very very simple webhook written in python for coding.net repos events
+
+## 目的
+有几个基于Django的项目，需要自动部署。代码库在Coding.net上。Coding.Net本身提供了Webhook机制。所以需要在服务器上部署webhook handler。
+目前网上找到的webhook handler都太重，我需要一个轻量级的，可快速部署在阿里云linux主机上的webhook handler方案。
+
+## 技术要点
+
+1. 需要起进程。方案：用supervisor
+2. 需要起HTTP服务。方案：用uwsgi
+3. 需要处理POST请求，并在其中处理各种event。方案：用python写wsgi脚本
+4. 需要处理基于HASH签名的校验机制。方案：python
+
+## 详细步骤
+
+### 1、安装supervisor
+
+可以在ubuntu命令行，使用如下命令：
+
+    apt-get install supervisord
+    
+### 2、安装uwsgi
+
+使用如下命令：
+
+    apt-get install build-essential python-dev
+    
+然后使用pip安装uwsgi
+
+    pip install uwsgi
+
+### 3、写python脚本
+
+使用以下类似的python脚本。
+
+```python
+#!/usr/bin/python
+
+```
+    
---
+++ scripts/hook_handler.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+from wsgiref.simple_server import make_server
+import json
+import os
+
+
+def application(environ, start_response):
+    # POST or GET ?
+    method = environ['REQUEST_METHOD']
+    response_body = ""
+    status = '200 OK'
+
+    if method == "POST":
+        # the environment variable CONTENT_LENGTH may be empty or missing
+        try:
+            request_body_size = int(environ.get('CONTENT_LENGTH', 0))
+        except (ValueError):
+            request_body_size = 0
+
+        # When the method is POST the variable will be sent
+        # in the HTTP request body which is passed by the WSGI server
+        # in the file like wsgi.input environment variable.
+        request_body = environ['wsgi.input'].read(request_body_size)
+        data = json.loads(request_body)
+
+        response_body = ""
+    elif method == "GET":
+        response_body = ""
+
+    response_headers = [
+        ('Content-Type', 'text/html'),
+        ('Content-Length', str(len(response_body)))
+    ]
+
+    start_response(status, response_headers)
+    return [response_body]
+
+
+if __name__ == "__main__":
+    # for test purpose only
+    httpd = make_server('localhost', 8051, application)
+    httpd.serve_forever()
